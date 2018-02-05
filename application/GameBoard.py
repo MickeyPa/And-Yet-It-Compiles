fed2722cb0ea4e5a5adfbb8afcfa4febacfc92f8
Cheema,Nimrat <nm-ch@hotmail.com>
2018-02-05 15:18:16 -0500

    #7 added position_map that maps positions to tuple indices

--- application/GameBoard.py
+++ application/GameBoard.py
@@ -1,3 +1,4 @@
+import string
 class GameBoard:
     # emptyposition is a tuple containing the index values of the empty slot's position on the gameboard.
 
@@ -8,12 +9,17 @@ class GameBoard:
     # indices = [[(0,0),(0,1),(0,2),(0,3),(0,4)],
     #            [(1,0),(1,1),(1,2),(1,3),(1,4)],
     #            [(2,0),(2,1),(2,2),(2,3),(2,4)]]
-    def __init__(self,board=None):
+
+    #Maps position to indices
+    # Example: position_map['A']=(0,0)
+    position_map={a:b for a,b in zip([s for s in string.ascii_uppercase[0:24]],[(i,j) for i in range(0,3) for j in range(0,5)])}
+
+    def __init__(self,board=None,level=1):
         if board is None:
-            self.board=self.randomize_board()
+            self.board=self.randomize_board(level)
         else:
             self.board=board
         self.empty_position==(0,0)
 
-    def randomize_board(self):
+    def randomize_board(self,level):
         pass
