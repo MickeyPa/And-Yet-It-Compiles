16d9c743ec178ef544d01082c5da6fc548d2cec2
Unknown <ma_eghtesadi@yahoo.com>
2018-02-10 16:39:54 -0500

    #1 added the goal stated function and unit tests for it

--- tests/test_gameboard.py
+++ tests/test_gameboard.py
@@ -9,3 +9,13 @@ def test_init_board_from_string():
     game_board=GameBoard(string)
     flattened_string=''.join(list(flatten(game_board.board)))
     assert(flattened_string==string.replace(" ",""))
+
+def test_check_failed_goal_state():
+    string = "r e b w r b b b r r r b r b w"
+    game_board = GameBoard(string)
+    assert(False == game_board.check_goal_state())
+
+def test_check_succesfull_goal_state():
+    string = "r e b w r b b b r r r e b w r"
+    game_board = GameBoard(string)
+    assert(True == game_board.check_goal_state())
