d6dce4accad0bfeb6fced685f036d203f70cfdb7
Cheema,Nimrat <nm-ch@hotmail.com>
2018-02-12 15:24:48 -0500

    Added text to pieces

--- gui/game.js
+++ gui/game.js
@@ -5,8 +5,8 @@ var dummy=[{'pos':[0,0],'piece':'a'},{'pos':[0,1],'piece':'b'},{'pos':[0,2],'pie
                 {'pos':[2,0],'piece':'k'},{'pos':[2,1],'piece':'l'},{'pos':[2,2],'piece':'m'},{'pos':[2,3],'piece':'n'},{'pos':[2,4],'piece':'o'}]
 
 var margin = {top: 0, right:0, bottom: 0, left: 0},
-            width =330 - margin.left - margin.right,
-            height = 210 - margin.top - margin.bottom;
+            width =600 - margin.left - margin.right,
+            height = 370 - margin.top - margin.bottom;
 var x=d3.scaleLinear()
     .domain([0, 5])
     .range([30, width]);
@@ -35,24 +35,53 @@ function initGameBoard(data){
     var oned=function(i){
         return [Math.floor(i/5),i%5];
     }
-    var row=svg.selectAll("rect").data(data,d=>d.piece)
+    var g=svg.selectAll("g").data(data,d=>d.piece)
  
-    var row_update=row.transition()
-    .duration(750).attr("x",(d,i)=>x(oned(i)[1]))
+
+    var gEnter = g.enter().append("g");
+   
+    g.select(".r-piece").transition()
+        .duration(750).attr("x",(d,i)=>x(oned(i)[1]))
         .attr("y",(d,i)=>y(oned(i)[0]))
         .attr("display",d=>d.piece=='e'?'none':'')
-     
-    var row_enter=row.enter()
-        .append("rect")
-        .attr("class", "pieces")
+
+    g.select(".r-bot").transition()
+        .duration(750).attr("x",(d,i)=>x(oned(i)[1])+4)
+        .attr("y",(d,i)=>y(oned(i)[0])+88)
+        .attr("display",d=>d.piece=='e'?'none':'')
+    g.select(".piece-text").transition()
+    .duration(750)
+        .attr("x",(d,i)=>x(oned(i)[1]))
+        .attr("y",(d,i)=>y(oned(i)[0]))
+ 
+    gEnter.append("rect")
+        .attr("class", "r-piece")
         .attr("x",(d,i)=>x(oned(i)[1]))
         .attr("y",(d,i)=>y(oned(i)[0]))
         .attr("display",d=>d.piece=='e'?'none':'')
-        .attr("height",60)
-        .attr("width",60)
+        .attr("height",100)
+        .attr("width",100)
+        .attr("rx",16)
+        .attr("ry",16);
+    gEnter.append("rect")
+        .attr("class", "r-bot")
+        .attr("x",(d,i)=>x(oned(i)[1])+4)
+        .attr("y",(d,i)=>y(oned(i)[0])+88)
+        .attr("display",d=>d.piece=='e'?'none':'')
+        .attr("height",12)
+        .attr("width",92)
         .attr("rx",18)
-        .attr("ry",18);
+        .attr("ry",30);
+    gEnter.append("text").text("R")
+        .attr("display",d=>d.piece=='e'?'none':'')
+        .attr("x",(d,i)=>x(oned(i)[1])+40)
+        .attr("y",(d,i)=>y(oned(i)[0])+50)
+        .attr("class", "piece-text")
+
+   
+         
+    
    
 
-    console.log("done");
+   
 }
