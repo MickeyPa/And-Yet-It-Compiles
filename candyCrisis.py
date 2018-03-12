c7d17d6e730aab710730b577a77cbbe2df5d22da
Mickey P <anamika.pancholy@gmail.com>
2018-03-11 21:24:24 -0400

    Completed Import and run multiple games from a single input file (Sample_Data.txt). - Checks all games from the input file for playability. - Runs all games in a loop. If a game from the input file is not playable, shows the user the FIRST error found in its formatting.

--- candyCrisis.py
+++ candyCrisis.py
@@ -3,59 +3,124 @@
 ###########################################################################
 
 import ast  # used to convert str to tuple
+from application.GameBoard import GameBoard
 
 print()
 print("HELLO AND WELCOME TO CANDY CRISIS, THE MOST DELICIOUS GAME IN TOWN!")
-#print("Please specify the path to your gameboard configuration:")
-#gbFilePath = input()
+# print("Please specify the path to your gameboard configuration:")
+# gbFilePath = input()
 
-# !!!!! CALLS THE IMPORT GAMEBOARD FEATURE and creates gameboard object with it
+# Ask if import or level choice is desired?
 
-from application.GameBoard import GameBoard
-g=GameBoard()
+# Import file
+importFile = open("Sample_Data.txt", 'r', encoding="utf8")
+gameStrings = []
 
-print("Woohoo! This is your gameboard. Let's get playing!")
-turnNum = 0 # number of turns in the game
-goalState = False
-while not goalState:
-    turnNum += 1
-    #I did this for the gui
-    gui_board=list(map(list,list(zip([p for row in g.board_state_helper for p in row],[p for row in g.board for p in row]))))
-    print(gui_board)
-    #print(g.board[0])
-    #print(g.board[1])
-    #print(g.board[2])
-    #print()
+# Check if game is solvable. If it is, add it to gameStrings
+for string in importFile.read().split("\n"):
+    game = string.split(" ")
 
-    valid = False
-    # loop until they enter a valid input
-    while not valid:
-        e=g.get_empty_position()
-        s=g.successors()
-        print("Your possible moves are:")
-        print(s)
-        print()
-        print("[Turn #", turnNum, "] What is your move?")
-        nextMove=ast.literal_eval(input())
-        print()
+    # check if game has exactly fifteen pieces
+    exactNumPieces = True
+    if game.len() != 15:
+        exactNumPieces = False
 
-        # search set of successors to see if input is valid
-        if nextMove in s:
-                valid = True
+    # check if all pieces are single characters
+    allChars = True
+    for piece in game:
+        if piece.len() > 1:
+            allChars = False
+            break
 
-        if not valid:
-            print("Oopsie Daisy! Thats not a valid move. Let's try this again.")
-    # end [while not valid]
+    # check if game has ONLY 1 empty position e
+    numE = 0
+    for piece in game:
+        if piece == 'e':
+            numE += 1
 
-    g.move(nextMove)
-    print("Nice move!")
-    if g.check_goal_state():
-        goalState = True
+    # check if game contains AT LEAST 5 pairs of letters
+    pairs = []
+    numPairs = 0
+    for piece in game:
+        if pairs.__contains__(piece):
+            numPairs += 1
+            pairs.remove(piece)
+        else:
+            pairs.append(piece)
+
+    # if both are true, append string to gameStrings, else append a string describing the FIRST error found.
+    if (numE == 1) and (numPairs >= 5) and exactNumPieces and allChars:
+        gameStrings.append(string)
+    elif not exactNumPieces:
+        gameStrings.append(': Does not contain exactly fifteen pieces.')
+    elif numE != 1:
+        gameStrings.append(': Does not contain exactly one empty position.')
+    elif not allChars:
+        gameStrings.append(': Not all game pieces are single characters.')
     else:
-        print("Here is your new-and-improved gameboard.")
+        gameStrings.append(': Game is not solvable.')
 
-# end [while not goalState]
+# Close input file
+importFile.detach()
 
+# Start running games
+gameNum = 0
+for string in gameStrings:
+    gameNum += 1
 
-print("CONGRATULATIONS! YOU HAVE WON THE GAME! EUREKA! FELICITATIONS! HAPPY BIRTHDAY!")
+    # Check if game is NOT playable
+    if string[:1] == ':':
+        print("Game #" + (gameNum+1) + "is not playable" + string)
+        continue
+
+    g = GameBoard(string)
+
+    # ASK HERE if want to run automatic mode or not.
+
+    print("Here's game #" + gameNum + "!")
+    print("Woohoo! This is your gameboard. Let's get playing!")
+    turnNum = 0  # number of turns in the game
+    goalState = False
+    while not goalState:
+        turnNum += 1
+        # I did this for the gui
+        gui_board=list(map(list,list(zip([p for row in g.board_state_helper for p in row],[p for row in g.board for p in row]))))
+        print(gui_board)
+        # print(g.board[0])
+        # print(g.board[1])
+        # print(g.board[2])
+        # print()
+
+        valid = False
+        # loop until they enter a valid input
+        while not valid:
+            e=g.get_empty_position()
+            s=g.successors()
+            print("Your possible moves are:")
+            print(s)
+            print()
+            print("[Turn #", turnNum, "] What is your move?")
+            nextMove=ast.literal_eval(input())
+            print()
+
+            # search set of successors to see if input is valid
+            if nextMove in s:
+                    valid = True
+
+            if not valid:
+                print("Oopsie Daisy! Thats not a valid move. Let's try this again.")
+        # end [while not valid]
+
+        g.move(nextMove)
+        print("Nice move!")
+        if g.check_goal_state():
+            goalState = True
+        else:
+            print("Here is your new-and-improved gameboard.")
+
+    # end [while not goalState]
+
+    print("CONGRATULATIONS! YOU HAVE WON THE GAME! EUREKA! FELICITATIONS! HAPPY BIRTHDAY!")
+
+# end [for string in gameStrings]
 
